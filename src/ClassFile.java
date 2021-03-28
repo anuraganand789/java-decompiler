ecccefc932add1aa4485cc57f8e01b70861ebe0a
Anurag Anand <anuraganand789@hotmail.com>
2021-03-28 20:53:34 +0530

    this file will store all the class information

---
+++ src/ClassFile.java
@@ -0,0 +1,17 @@
+import cpinfo.ConstantPoolInfo;
+
+public class ClassFile{
+    //#CAFEBABE
+    public int                magicNumber;
+
+    public int                minorVersion;
+    public int                majorVersion;
+
+    public int                constantPoolCount;
+    public ConstantPoolInfo[] constantPool;
+
+    public int                accessFlags;
+    public int                thisClass;
+    public int                superClass;
+    public int                interfacesCount;
+}
