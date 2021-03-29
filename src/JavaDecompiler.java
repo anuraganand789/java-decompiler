4f7f4a5f776f519eb165fb0e07b15c3f9782eb9d
Anurag Anand <anuraganand789@hotmail.com>
2021-03-30 00:48:39 +0530

    1. sort the imports 2. store all information in constant pool 3. use constant pool to print those information 4. removed sysout statements from print constant pool info 5. print number of interfaces 6. print name of thisClass 7. print name of superclass

--- src/JavaDecompiler.java
+++ src/JavaDecompiler.java
@@ -6,46 +6,53 @@ import java.io.FileNotFoundException;
 
 import java.io.IOException;
 
+import java.util.Date;
+
 import constants.ReferenceKind;
 
-import cpinfo.ConstantPoolInfo;
-import cpinfo.MethodRefInfo;
-import cpinfo.Utf8Info;
-import cpinfo.IntegerInfo;
 import cpinfo.ClassInfo;
-import cpinfo.FloatInfo;
+import cpinfo.ConstantPoolInfo;
 import cpinfo.DoubleInfo;
-import cpinfo.LongInfo;
-import cpinfo.StringInfo;
+import cpinfo.DynamicInfo;
 import cpinfo.FieldRefInfo;
+import cpinfo.FloatInfo;
+import cpinfo.IntegerInfo;
 import cpinfo.InterfaceMethodRefInfo;
+import cpinfo.InvokeDynamicInfo;
+import cpinfo.LongInfo;
 import cpinfo.MethodHandleInfo;
+import cpinfo.MethodRefInfo;
+import cpinfo.MethodTypeInfo;
+import cpinfo.ModuleInfo;
 import cpinfo.NameAndTypeInfo;
+import cpinfo.PackageInfo;
+import cpinfo.StringInfo;
+import cpinfo.Utf8Info;
 
-import static constants.ConstantTypes.CONSTANT_Utf8                ;
-import static constants.ConstantTypes.CONSTANT_Integer             ;
-import static constants.ConstantTypes.CONSTANT_Float               ;
-import static constants.ConstantTypes.CONSTANT_Long                ;
-import static constants.ConstantTypes.CONSTANT_Double              ;
 import static constants.ConstantTypes.CONSTANT_Class               ;
-import static constants.ConstantTypes.CONSTANT_String              ;
-import static constants.ConstantTypes.CONSTANT_Fieldref            ;
-import static constants.ConstantTypes.CONSTANT_MethodRef           ;
-import static constants.ConstantTypes.CONSTANT_InterfaceMethodRef  ;
-import static constants.ConstantTypes.CONSTANT_NameAndType         ;
-import static constants.ConstantTypes.CONSTANT_MethodHandle        ;
-import static constants.ConstantTypes.CONSTANT_MethodType          ;
+import static constants.ConstantTypes.CONSTANT_Double              ;
 import static constants.ConstantTypes.CONSTANT_Dynamic             ;
+import static constants.ConstantTypes.CONSTANT_Fieldref            ;
+import static constants.ConstantTypes.CONSTANT_Float               ;
+import static constants.ConstantTypes.CONSTANT_Integer             ;
+import static constants.ConstantTypes.CONSTANT_InterfaceMethodRef  ;
 import static constants.ConstantTypes.CONSTANT_InvokeDynamic       ;
+import static constants.ConstantTypes.CONSTANT_Long                ;
+import static constants.ConstantTypes.CONSTANT_MethodHandle        ;
+import static constants.ConstantTypes.CONSTANT_MethodRef           ;
+import static constants.ConstantTypes.CONSTANT_MethodType          ;
 import static constants.ConstantTypes.CONSTANT_Module              ;
+import static constants.ConstantTypes.CONSTANT_NameAndType         ;
 import static constants.ConstantTypes.CONSTANT_Package             ;
+import static constants.ConstantTypes.CONSTANT_String              ;
+import static constants.ConstantTypes.CONSTANT_Utf8                ;
 
 public class JavaDecompiler{
     private static String spacePadding = "                    ";
     
     private static ConstantPoolInfo[] printConstantPool(final DataInputStream dataInputStream) throws IOException{
         final int numberOfItemsInConstantPool = dataInputStream.readUnsignedShort();
-        final ConstantPoolInfo[] constantPool = new ConstantPoolInfo[numberOfItemsInConstantPool];
+        final ConstantPoolInfo[] constantPool = new ConstantPoolInfo[numberOfItemsInConstantPool - 1];
         System.out.println("Constant Pool : ");
 
         for(int constantIndex = 1; constantIndex < numberOfItemsInConstantPool; ++constantIndex){
@@ -59,29 +66,16 @@ public class JavaDecompiler{
 
                 constantPool[constantIndex - 1] = methodRefInfo;
 
-                System.out.format("#%-2d = MethodRef %s #%d.#%d %n", 
-                                  constantIndex, 
-                                  spacePadding,
-                                  methodRefInfo.classIndex(),
-                                  methodRefInfo.nameAndTypeIndex());
 
             break;
             case CONSTANT_Utf8 : 
                 final Utf8Info utf8Info = new Utf8Info(dataInputStream.readUTF());
                 constantPool[constantIndex - 1] = utf8Info;
-                System.out.format("#%2d = Utf8 %s %s%n", 
-                                  constantIndex, 
-                                  spacePadding,
-                                  utf8Info.utf8Value());
                 break;
             case CONSTANT_Integer : 
                 final IntegerInfo integerInfo   = new IntegerInfo(dataInputStream.readInt());
                 constantPool[constantIndex - 1] = integerInfo;
 
-                System.out.format("#%2d = Integer %s %d%n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  integerInfo.intValue());
                 break;
 
             case CONSTANT_Class: 
@@ -89,47 +83,27 @@ public class JavaDecompiler{
                 classInfo = new ClassInfo(dataInputStream.readUnsignedShort());
                 constantPool[constantIndex - 1] = classInfo;
 
-                System.out.format("#%2d = Class %s #%d%n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  classInfo.nameIndex());
                 break;
            case CONSTANT_Float              : 
                 final FloatInfo floatInfo = new FloatInfo(dataInputStream.readFloat());
                 constantPool[constantIndex - 1] = floatInfo;
 
-                System.out.format("#%2d = Float %s %f%n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  floatInfo.floatValue());
            break;
            case CONSTANT_Long               : 
                 final LongInfo longInfo = new LongInfo(dataInputStream.readLong());
                 constantPool[constantIndex - 1] = longInfo;
                 
-                System.out.format("#%2d = Integer %s %d%n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  longInfo.longValue());
            break;
            case CONSTANT_Double             : 
                 final DoubleInfo doubleInfo     = new DoubleInfo(dataInputStream.readDouble());
                 constantPool[constantIndex - 1] = doubleInfo;
 
-                System.out.format("#%2d = Integer %s %f%n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  doubleInfo.doubleValue());
            break;
            case CONSTANT_String             : 
                 final StringInfo 
                 stringInfo = new StringInfo(dataInputStream.readUnsignedShort());
                 constantPool[constantIndex - 1] = stringInfo;
 
-                System.out.format("#%2d = String %s #%d %n", 
-                                  constantIndex, 
-                                  spacePadding, 
-                                  stringInfo.stringIndex());
            break;
            case CONSTANT_Fieldref           : 
                final FieldRefInfo 
@@ -137,22 +111,12 @@ public class JavaDecompiler{
                                                dataInputStream.readUnsignedShort());
                constantPool[constantIndex - 1] = fieldRefInfo;
 
-               System.out.format("#%2d = FieldRef %s #%d.#%d %n", 
-                                 constantIndex, 
-                                 spacePadding, 
-                                 fieldRefInfo.classIndex(),
-                                 fieldRefInfo.nameAndTypeIndex());
                break;
            case CONSTANT_InterfaceMethodRef : 
                final InterfaceMethodRefInfo 
                interfaceMethodRef = new InterfaceMethodRefInfo(dataInputStream.readUnsignedShort(),
                                                            dataInputStream.readUnsignedShort());
                constantPool[constantIndex - 1] = interfaceMethodRef;
-               System.out.format("#%2d = InterfaceMethodRef %s #%d.#%d %n", 
-                                 constantIndex, 
-                                 spacePadding, 
-                                 interfaceMethodRef.classIndex(), 
-                                 interfaceMethodRef.nameAndTypeIndex());
                break;
            case CONSTANT_NameAndType        : 
                final NameAndTypeInfo 
@@ -160,45 +124,29 @@ public class JavaDecompiler{
                                                  dataInputStream.readUnsignedShort());
                constantPool[constantIndex - 1] = nameAndType;
 
-               System.out.format("#%2d = NameAndType %s #%d.#%d %n", 
-                                 constantIndex, 
-                                 spacePadding, 
-                                 nameAndType.nameIndex(), 
-                                 nameAndType.descriptorIndex());
            break;
            case CONSTANT_MethodHandle       : 
                final MethodHandleInfo 
                methodHandle = new MethodHandleInfo(dataInputStream.readUnsignedByte(),
                                                   dataInputStream.readUnsignedShort());
-               System.out.format("#%2d = MethodHandle %s #%d.#%d %s %n", 
-                                 constantIndex, 
-                                 spacePadding, 
-                                 methodHandle.referenceKind(), 
-                                 methodHandle.referenceIndex(),
-                                 ReferenceKind.getKind(methodHandle.referenceKind()));
            break;
 
            case CONSTANT_MethodType         : 
-               final int method_type_descriptor_index = dataInputStream.readUnsignedShort();
-               System.out.format("#%2d = MethodType %s #%d %n" , constantIndex, spacePadding, method_type_descriptor_index);
+               final MethodTypeInfo methodType = new MethodTypeInfo(dataInputStream.readUnsignedShort());
            break;
            case CONSTANT_Dynamic            : 
-               final int dynamic_bootstrap_method_attr_index  = dataInputStream.readUnsignedShort();
-               final int dynamic_name_and_type_index          = dataInputStream.readUnsignedShort();
-               System.out.format("#%2d = DynamicInfo %s #%d.#%d %n" , constantIndex, spacePadding, dynamic_bootstrap_method_attr_index, dynamic_name_and_type_index);
+               final DynamicInfo dynamic = new DynamicInfo(dataInputStream.readUnsignedShort(),
+                                                           dataInputStream.readUnsignedShort());
                break;
            case CONSTANT_InvokeDynamic      : 
-               final int invoke_dynamic_bootstrap_method_attr_index  = dataInputStream.readUnsignedShort();
-               final int invoke_dynamic_name_and_type_index          = dataInputStream.readUnsignedShort();
-               System.out.format("#%2d = InvokeDynamicInfo %s #%d.#%d %n" , constantIndex, spacePadding, invoke_dynamic_bootstrap_method_attr_index, invoke_dynamic_name_and_type_index);
+               final InvokeDynamicInfo invokeDynamic = new InvokeDynamicInfo(dataInputStream.readUnsignedShort(),
+                                                                             dataInputStream.readUnsignedShort());
                break;
            case CONSTANT_Module             : 
-               final int module_name_index = dataInputStream.readUnsignedShort();
-               System.out.format("#%2d = Module %s #%d %n" , constantIndex, spacePadding, module_name_index);
+               final ModuleInfo moduleInfo = new ModuleInfo(dataInputStream.readUnsignedShort());
            break;
            case CONSTANT_Package            : 
-               final int package_name_index = dataInputStream.readUnsignedShort();
-               System.out.format("#%2d = Package %s #%d %n" , constantIndex, spacePadding, package_name_index);
+               final PackageInfo packageInfo = new PackageInfo(dataInputStream.readUnsignedShort());
            break;
 
             default: 
@@ -224,15 +172,25 @@ public class JavaDecompiler{
          try(final FileInputStream fileInputStream = new FileInputStream(file);
              final DataInputStream dataInputStream = new DataInputStream(fileInputStream)){
              classFile.magicNumber = dataInputStream.readInt();
-             System.out.println("magic - " + classFile.magicNumber);
+             if(classFile.magicNumber != 0xCAFEBABE) {
+                 throw new IllegalArgumentException("Invalid Class File");
+             }
+
+             System.out.format("Classfile %s%n", file.getAbsolutePath());
+             System.out.format("Last modified %s; size %d bytes%n", 
+                                new Date(file.lastModified()),
+                                file.length());
 
              classFile.minorVersion = dataInputStream.readUnsignedShort();
-             System.out.println("Minor Version   - " + classFile.minorVersion);
-
              classFile.majorVersion = dataInputStream.readUnsignedShort();
-             System.out.println("Major Version  - " + classFile.majorVersion);
 
              classFile.constantPool = printConstantPool(dataInputStream);
+
+             classFile.accessFlags = dataInputStream.readUnsignedShort();
+             classFile.thisClass   = dataInputStream.readUnsignedShort();
+             classFile.superClass  = dataInputStream.readUnsignedShort();
+             classFile.interfacesCount = dataInputStream.readUnsignedShort();
+
          } catch(FileNotFoundException ex){
              ex.printStackTrace();
          }catch(IOException ex){
@@ -240,5 +198,48 @@ public class JavaDecompiler{
          }
 
 
+         final String paddingOf10 = " ".repeat(10);
+         final String paddingOf24 = " ".repeat(24);
+         System.out.println("Class");
+         System.out.format(" minor version: %d%n", classFile.minorVersion);
+         System.out.format(" major version: %d%n", classFile.majorVersion);
+         System.out.format(" flags : (%d) %n",     classFile.accessFlags);
+
+         final ClassInfo classInfoThisClass = (ClassInfo) classFile.constantPool[classFile.thisClass - 1];
+         final Utf8Info thisClassUtf8       = (Utf8Info)  classFile.constantPool[classInfoThisClass.nameIndex() - 1];
+         System.out.format(" this_class : #%d %s // %s %n", 
+                             classFile.thisClass,
+                             paddingOf24, 
+                             thisClassUtf8.utf8Value());
+
+         final ClassInfo classInfoSuperClass = (ClassInfo) classFile.constantPool[classFile.superClass - 1];
+         final Utf8Info  superClassUtf8       = (Utf8Info) classFile.constantPool[classInfoSuperClass.nameIndex() - 1];
+         System.out.format(" super_class : #%d %s // %s %n", 
+                             classFile.superClass,
+                             paddingOf24, 
+                             superClassUtf8.utf8Value());
+
+         System.out.format("""
+                              %s interfaces: %d, \
+                              fields: %d \
+                              methods: %d, \
+                              attributes: %d \
+                              %n""".stripIndent(), 
+                              "",
+                              classFile.interfacesCount,
+                              0,
+                              0,
+                              0);
+
+         System.out.println("Constant Pool:");
+         int constantIndex = 1;
+         for(ConstantPoolInfo constantPoolItem : classFile.constantPool){
+             System.out.format("%3s = %s ", 
+                               "#"+constantIndex, 
+                               constantPoolItem.value()
+                               );
+             ++constantIndex;
+         }
+         System.out.println();
     }
 }
